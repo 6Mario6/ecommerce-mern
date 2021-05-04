e8c9573e5e572ddab08321ddcd533e9251a683a5
Mario <jorge.fernandez@pragma.com.co>
2021-05-03 21:51:17 -0500

    added the routing for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
-
+import ProductScreen from './screens/ProductScreen'
 const App = () => {
   
   return (
-    <>
-    <Header></Header>
-    <main>
-      <Container>
-        <HomeScreen></HomeScreen>
-      </Container>
-     
-     </main>
-     <Footer></Footer>
-    </>
-  )
+    <Router>
+      <Header></Header>
+      <main>
+        <Container>
+          <Route path="/" component={HomeScreen} exact/>
+          <Route path="/product/:id" component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </Router>
+  );
 }
 
 export default App
